2e0ea81b397de67c05f69f91e666e71c8f8685fa
Daniel <danielood@hotmail.es>
2021-02-11 10:35:35 +0100

    Crear Proyecto

---
+++ src/main/java/Cliente/Main/LanzadorCliente.java
@@ -0,0 +1,8 @@
+package Cliente.Main;
+
+public class LanzadorCliente {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/Servidor/Main/LanzadorServer.java
@@ -0,0 +1,8 @@
+package Servidor.Main;
+
+public class LanzadorServer {
+
+    public static void main(String[] args){
+
+    }
+}
